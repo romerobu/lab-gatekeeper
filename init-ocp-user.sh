eb0479b46df77e9fef3d7ba93144e0edb972e172
Coral Romero Buzon <cromerob@redhat.com>
2021-06-14 22:00:06 +0200

    Update templates

--- init-ocp-user.sh
+++ init-ocp-user.sh
@@ -37,6 +37,30 @@ echo "OAuth should has been now updated"
 #     oc new-project $ns
 # done
 
+# Set up gatekeeper operator
+
+oc apply -f lab-gatekeeper-files/petclinic/install-operator.yaml
+
+echo -n "Waiting for pods ready..."
+while [[ $(oc get pods  -n openshift-operators  -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
+
+sleep 5
+
+# Create gatekeeper instance
+
+echo -n "Creating gatekeeper..."
+oc apply -f lab-gatekeeper-files/petclinic/create-gatekeeper.yaml
+
+echo -n "Waiting for pods ready..."
+while [[ $(oc get pods  -n openshift-gatekeeper-system  -o 'jsonpath={..status.conditions[?(@.type=="Ready")].status}') != "True True True" ]]; do echo -n "." && sleep 1; done; echo -n -e "  [OK]\n"
+
+sleep 5
+
+# Deploy gatekeeper constraints and templates
+
+echo -n "Deploying config..."
+oc apply -f lab-gatekeeper-files/petclinic/config.yaml
+
 # Create namespace and add roles
 oc get secret htpasswd -n openshift-config -o yaml \
   | grep htpasswd | grep -v "{}" \
@@ -52,6 +76,7 @@ do
     oc adm policy add-cluster-role-to-user gatekeeper-ops-role $user
     oc adm policy add-role-to-user admin $user -n petclinic-bluegreen-$user
     oc adm policy add-role-to-user view $user -n openshift-gatekeeper-system
+    oc adm policy add-role-to-user view $user -n openshift-operators 
     oc adm policy add-role-to-user admin $user -n petclinic-beta-$user
     # create resource quota in beta namespace
     oc apply -f lab-gatekeeper-files/lab5/rs.yaml -n petclinic-beta-$user
