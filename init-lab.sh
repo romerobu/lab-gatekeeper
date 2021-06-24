3b20ae8415b35aed5f21d3be4676ae58089bc243
Coral Romero Buzon <cromerob@redhat.com>
2021-06-24 11:00:55 +0200

    First commit

--- init-lab.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-oc apply -f lab-gatekeeper-files/lab1/constraintTemplate.yaml
-oc apply -f lab-gatekeeper-files/lab2/constraintTemplateDeployment.yaml
-oc apply -f lab-gatekeeper-files/lab2/constraintTemplateRoute.yaml
-oc apply -f lab-gatekeeper-files/lab2/constraintTemplateSvc.yaml
-oc apply -f lab-gatekeeper-files/lab3/constraintTemplate.yaml
-oc apply -f lab-gatekeeper-files/lab4/constraintTemplate.yaml
-oc apply -f lab-gatekeeper-files/lab5/constraintTemplate.yaml
